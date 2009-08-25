0f28b842803125e3fe4a35c2e7c422a21755974c
beepscore <support@beepscore.com>
2009-08-24 21:46:42 -0700

    Add PersonListViewController.  Didn't change nib yet.

---
+++ Classes/PersonListViewController.h
@@ -0,0 +1,16 @@
+//
+//  PersonListViewController.h
+//  Presence
+//
+//  Created by Steve Baker on 8/24/09.
+//  Copyright 2009 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PersonListViewController : UITableViewController {
+
+}
+
+@end
